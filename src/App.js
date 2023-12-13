b5ecf015c57c0d6e2cf9d9aae8516e60a0f96f30
Ryosuke Fuse <ryosuke.fuse@fullmarks.co.jp>
2023-12-13 23:38:09 +0900

    JSX tutorial

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +17,37 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Profile />
+      <MyButton />
     </div>
   );
 }
+
+function MyButton() {
+  return <button>test</button>;
+}
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        src={user.imageUrl}
+        alt={'test image' + user.imageName}
+        style={{ width: user.imageSize, height: user.imageSize }}
+      />
+      <br />
+    </>
+  );
+}
+
+const user = {
+  name: 'Test Name',
+  imageUrl:
+    'https://frame-illust.com/fi/wp-content/uploads/2019/02/reiwa-happyou.png',
+  imageSize: 120,
+  imageName: 'reiwa',
+};
 
 export default App;
